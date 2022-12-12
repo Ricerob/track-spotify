4e9486d974f77ad026677e88aed0938b69f60a8b
Robby <riceslif@gmail.com>
2022-12-11 19:04:05 -0600

    Taking silly console.logs out

--- src/api/checkIfValid.js
+++ src/api/checkIfValid.js
@@ -2,8 +2,6 @@
 
 // Call https://api.spotify.com/v1/artists/test1, if 400 return false
 export default async function checkIfValid(id, access_token) {
-    console.log(id)
-    console.log(access_token)
 
     const response = await fetch(`https://api.spotify.com/v1/artists/${id}`, {
         method: 'GET',
